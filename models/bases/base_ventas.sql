b1a2eeca5275b6612a4ca6c18167fd5a64507b1a
Nuria Cano <alumnodbt25@gmail.com>
2025-05-18 19:50:40 +0000

    commit

--- models/bases/base_ventas.sql
+++ models/bases/base_ventas.sql
@@ -1,9 +1,5 @@
 with ventas as (
-    select * from {{source('trabajo_final','ventas')}}
-)
-
-select 
-    {{ dbt_utils.generate_surrogate_key(['nombre','ventas_globales']) }} as ventas_id,
+    select 
     lower(nombre) as videojuego,
     lower(plataforma) as consola,
     year_published as lanzamiento,
@@ -17,5 +13,24 @@ select
     otras_ventas,
     ventas_globales,
     reviews as critica
+    
+     from {{source('trabajo_final','ventas')}}
+)
+
+select 
+    {{ dbt_utils.generate_surrogate_key(['videojuego','ventas_globales']) }} as ventas_id,
+    videojuego,
+    consola,
+    lanzamiento,
+    genero,
+    empresa,
+    precio_dolares,
+    precio_euros,
+    ventas_na,
+    ventas_eu,
+    ventas_jp,
+    otras_ventas,
+    ventas_globales,
+    critica
 
 from ventas
