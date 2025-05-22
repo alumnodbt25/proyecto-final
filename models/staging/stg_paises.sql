7765367ec5717a0a78efe4087659b1ebf0699b6a
Nuria Cano <alumnodbt25@gmail.com>
2025-05-22 10:27:48 +0000

    commit

--- models/staging/stg_paises.sql
+++ models/staging/stg_paises.sql
@@ -1,12 +1,12 @@
 with paises as (
     select 
-    lower(pais) as pais,
-    lower(country) as country,
-    lower(pays) as pays,
-    abreviatura_1,
-    abreviatura_2,
-    codigo_telf,
-    lower(continente) as continente
+    cast(lower(pais) as varchar(256)) as pais,
+    cast(lower(country) as varchar(256)) as country,
+    cast(lower(pays) as varchar(256)) as pays,
+    cast(abreviatura_1 as varchar(4)) as abreviatura_1,
+    cast(abreviatura_2 as varchar(4)) as abreviatura_2,
+    cast(replace(codigo_telf,' ','') as integer) as codigo_telf,
+    cast(lower(continente) as varchar(256)) as continente
     
     from {{source("trabajo_final",'paises')}}
 )
